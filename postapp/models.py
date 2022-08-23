b8d66afecb6b81f090c579d732410b4693f9302a
RishavYaduvanshi <rishavkhiladi768@gmail.com>
2022-08-23 14:40:49 +0530

    update commit

--- postapp/models.py
+++ postapp/models.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.db import models
 
 
 class Post(models.Model):
+    user = models.ForeignKey('authn.User', on_delete=models.CASCADE)
     url = models.URLField(  )
     caption = models.CharField(max_length=200)
     liked_by = models.ManyToManyField('authn.User', related_name='likes')
--- postapp/serializer.py
+++ postapp/serializer.py
@@ -1,13 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SerializerMethodField
+
+from authn.serializers import UserSerializer
 from .models import Post
 
 class PostSerializer(ModelSerializer):
+    
+    def get_userName(self, obj):
+        return str(obj.user.username)
+    userName = SerializerMethodField()
+    
     class Meta:
         model = Post
-        fields = ('url', 'caption', 'liked_by', 'created_at', 'updated_at','pk')
+        
+        fields = ('url', 'caption', 'liked_by', 'created_at', 'updated_at','pk', 'userName')
         read_only_fields = ('created_at', 'updated_at')
         extra_kwargs = {
             'url': {'required': True},
             'caption': {'required': True},
         }
+
     
