114fb3271e53d81fa2b0e78c1d4531a5def61e85
Gagandeep Singh Bhambrah <19cse223.gagandeepsinghbhambrah@giet.edu>
2022-09-26 13:25:03 +0530

    fix

--- postapp/serializer.py
+++ postapp/serializer.py
@@ -6,9 +6,6 @@ class PostSerializer(ModelSerializer):
     
     def get_userName(self, obj):
         return str(obj.user.username)
-    
-    def get_userDp(self, obj):
-        return str(obj.user.dp)
 
 
 
@@ -29,7 +26,7 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         
-        fields = ('url', 'caption', 'liked_cnt', 'created_at', 'updated_at','pk', 'userName','user','likedStatus','userDp')
+        fields = ('url', 'caption', 'liked_cnt', 'created_at', 'updated_at','pk', 'userName','user','likedStatus','user__dp')
         read_only_fields = ('created_at', 'updated_at')
         extra_kwargs = {
             'url': {'required': True},
